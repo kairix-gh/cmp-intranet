4baea41be6e6c778606b6d7680861bc5b60908bc
Kairix <kairix@teameligere.com>
2021-05-15 19:17:45 -0600

    - Remove dupliate style

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -131,7 +131,7 @@
                             </PopoverButton>
 
                             <transition enter-active-class="transition duration-200 ease-out" enter-from-class="translate-y-1 opacity-0" enter-to-class="translate-y-0 opacity-100" leave-active-class="transition duration-150 ease-in" leave-from-class="translate-y-0 opacity-100" leave-to-class="translate-y-1 opacity-0" >
-                                <PopoverPanel focus class="absolute z-10 w-screen left-0 mt-5 z-10">
+                                <PopoverPanel focus class="absolute z-10 w-screen left-0 mt-5">
                                     <div class="overflow-hidden shadow-lg ring-1 ring-black ring-opacity-5 bg-white max-w-7xl mx-auto rounded">
 
                                         <form @submit.prevent="search" class="w-full flex rounded border focus-within:ring-1 ring-primary p-2">
