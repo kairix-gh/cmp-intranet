1296fd35746d8bd47ef06ea073034b76352a423a
Kairix <kairix@teameligere.com>
2021-05-15 18:48:54 -0600

    - Add a see all news buttom

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,6 +26,11 @@
                     </div>
                 </a>
             </div>
+
+            <a href="#" class="inline-flex items-center mt-4 uppercase text-white text-sm font-medium bg-blue-300 rounded px-2.5 py-1 hover:bg-blue-600 w-full md:w-auto justify-center md:self-end">
+                See All News
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" /></svg>
+            </a>
         </div>
     </div>
 </template>
