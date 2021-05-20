1fe993a610b0a7239c48be1c3062e95ce57dce73
Kairix <kairix@teameligere.com>
2021-05-19 19:20:54 -0600

    - Fix not providing ItemsOnFirsrtpage and ItemsPerPage to Pagination on News view. - Fix providing 1 extra count of items to Pagination on News view.

--- src/views/News.vue
+++ src/views/News.vue
@@ -6,7 +6,7 @@
             <NewsPostComponent :Post="item" v-for="item in newsPosts.slice(arrayStart, arrayEnd)" :key="item" />
         </div>
 
-        <Pagination :TotalItems="newsPosts.length" :CurrentPage="currentPage" v-on:requestPageChange="requestPageChangeEvent" />
+        <Pagination :TotalItems="newsPosts.length-1" :CurrentPage="currentPage" :ItemsOnFirstPage="itemsOnFirstPage" :ItemsPerPage="itemsPerPage" v-on:requestPageChange="requestPageChangeEvent" />
     </div>
 </template>
 
