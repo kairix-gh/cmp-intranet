dfc500039c92c13acf81138c318f8464bd383134
Kairix <kairix@teameligere.com>
2021-05-21 19:53:31 -0600

    - Add print and screen media queries to tailwind - Add print support to Member Arrivals. This opens a new window for printing because elements that are display: fixed do not print correctly in any browser.

--- src/views/HotelPanelContent/LoyaltyMemberArrivals.vue
+++ src/views/HotelPanelContent/LoyaltyMemberArrivals.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex justify-end">
-        <button class="inline-block bg-blue-300 hover:bg-blue-600 text-white rounded px-2.5 py-1 transition duration-300 form-base-focus">Print PDF</button>
+    <div class="flex justify-end print:hidden">
+        <button @click.prevent="printReport" class="inline-block bg-blue-300 hover:bg-blue-600 text-white rounded px-2.5 py-1 transition duration-300 form-base-focus">Print PDF</button>
     </div>
-    <div class="flex flex-col w-full">
+    <div ref="printContent" class="flex flex-col w-full">
         <div class="flex flex-col mt-4">
             <div class="overflow-x-auto">
                 <div class="py-2 align-middle inline-block min-w-full">
@@ -11,28 +11,28 @@
                             <thead class="bg-gray-50">
                                 <tr>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Member Level</th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Point Balance</th>
+                                    <th scope="col" class="hidden md:table-cell print:table-cell px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Member Id</th>
+                                    <th scope="col" class="hidden md:table-cell print:hidden px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Point Balance</th>
                                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Stay Dates</th>
-                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"><span class="sr-only">View</span></th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider print:hidden"><span class="sr-only">View</span></th>
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
                                 <template v-for="tier in memberTiers" :key="tier">
                                     <tr v-if="members.filter(m => m.level == tier.name).length > 0">
-                                        <td class="" colspan="5" :style="tier.style">
+                                        <td class="" colspan="6" :style="tier.style">
                                             <p class="text-center uppercase font-medium text-sm text-gray-100">{{ tier.name }} Member Arrivals</p>
                                         </td>
                                     </tr>
                                     <tr v-for="member in members.filter(m => m.level == tier.name)" :key="member" class="hover:bg-blue-50">
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             <div class="text-sm text-gray-900 font-medium">{{ member.name }}</div>
-                                            <div class="text-sm text-gray-500">{{ member.email }}</div>
+                                            <div class="text-sm text-gray-500 print:hidden">{{ member.email }}</div>
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            {{ member.level }}
+                                        <td class="px-6 py-4 whitepsace-nowrap">
+                                            {{ member.id }}
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
+                                        <td class="px-6 py-4 whitespace-nowrap hidden md:table-cell print:hidden">
                                             {{ formatNumber(member.balance) }} pts.
                                         </td>
                                         <td class="px-6 py-4 whitespace-nowrap text-sm space-y-1">
@@ -43,7 +43,7 @@
                                                 D: {{ member.departure }}
                                             </div>
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap">
+                                        <td class="px-6 py-4 whitespace-nowrap print:hidden">
                                             <a href="#" class="text-blue-600 hover:text-blue-900">View</a>
                                         </td>
                                     </tr>
@@ -58,8 +58,24 @@
     </div>
 </template>
 
+<style scoped>
+table {
+    page-break-inside: auto;
+}
+
+thead {
+    display: table-header-group;
+}
+
+tr {
+    page-break-before: always;
+    page-break-inside: avoid;
+    page-break-after: always;
+}
+</style>
+
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 import "@/assets/css/form-styles.css";
 
@@ -76,27 +92,66 @@ export default defineComponent({
         ]
 
         const members = [
-            { name: "Sanna Kipling", email: "sanna.kipling@example.com", level: "Silver", balance: 6255,  arrival: "5/1/2021", departure: "5/4/2021" },
-            { name: "Tasha Salminen", email: "tasha.salminen@example.com", level: "Platinum", balance: 12698,  arrival: "5/1/2021", departure: "5/6/2021" },
-            { name: "Carolyn Boothman", email: "carolyn.boothman@example.com", level: "Bronze", balance: 647,  arrival: "5/1/2021", departure: "5/3/2021" },
-            { name: "Eemeli Kaydence", email: "eemeli.kaydence@example.com", level: "Diamond", balance: 687326,  arrival: "5/1/2021", departure: "5/7/2021" },
-            { name: "Olen Sirianni", email: "Olen.Sirianni@example.com", level: "Bronze", balance: 322,  arrival: "5/1/2021", departure: "5/2/2021" },
-            { name: "Shonna Pouliot", email: "Shonna.Pouliot@example.com", level: "Iron", balance: 50,  arrival: "5/1/2021", departure: "5/2/2021" },
-            { name: "Ranae Gettinger", email: "Ranae.Gettinger@example.com", level: "Platinum", balance: 22854,  arrival: "5/1/2021", departure: "5/5/2021" },
-            { name: "Kyle Ragin", email: "Kyle.Ragin@example.com", level: "Iron", balance: 17,  arrival: "5/1/2021", departure: "5/2/2021" },
-            { name: "Zoraida Westerlund", email: "Zoraida.Westerlund@example.com", level: "Iron", balance: 62,  arrival: "5/1/2021", departure: "5/2/2021" },
-            { name: "Melina Herbst", email: "Melina.Herbst@example.com", level: "Iron", balance: 141,  arrival: "5/1/2021", departure: "5/2/2021" },
-            { name: "Irma Engelhard", email: "Irma.Engelhard@example.com", level: "Silver", balance: 1369,  arrival: "5/1/2021", departure: "5/4/2021" },
-            { name: "Otilia Mccree", email: "Otilia.Mccree@example.com", level: "Iron", balance: 102,  arrival: "5/1/2021", departure: "5/2/2021" },
-            { name: "Graham Duda", email: "Graham.Duda@example.com", level: "Bronze", balance: 899,  arrival: "5/1/2021", departure: "5/2/2021" },
-            { name: "Hilario Yeatman", email: "Hilario.Yeatman@example.com", level: "Iron", balance: 7,  arrival: "5/1/2021", departure: "5/2/2021" },
+            { name: "Sanna Kipling", id: "XXX-12-3456", email: "sanna.kipling@example.com", level: "Silver", balance: 6255,  arrival: "5/1/2021", departure: "5/4/2021" },
+            { name: "Tasha Salminen", id: "XXX-12-3456", email: "tasha.salminen@example.com", level: "Platinum", balance: 12698,  arrival: "5/1/2021", departure: "5/6/2021" },
+            { name: "Carolyn Boothman", id: "XXX-12-3456", email: "carolyn.boothman@example.com", level: "Bronze", balance: 647,  arrival: "5/1/2021", departure: "5/3/2021" },
+            { name: "Eemeli Kaydence", id: "XXX-12-3456", email: "eemeli.kaydence@example.com", level: "Diamond", balance: 687326,  arrival: "5/1/2021", departure: "5/7/2021" },
+            { name: "Olen Sirianni", id: "XXX-12-3456", email: "Olen.Sirianni@example.com", level: "Bronze", balance: 322,  arrival: "5/1/2021", departure: "5/2/2021" },
+            { name: "Shonna Pouliot", id: "XXX-12-3456", email: "Shonna.Pouliot@example.com", level: "Iron", balance: 50,  arrival: "5/1/2021", departure: "5/2/2021" },
+            { name: "Ranae Gettinger", id: "XXX-12-3456", email: "Ranae.Gettinger@example.com", level: "Platinum", balance: 22854,  arrival: "5/1/2021", departure: "5/5/2021" },
+            { name: "Kyle Ragin", id: "XXX-12-3456", email: "Kyle.Ragin@example.com", level: "Iron", balance: 17,  arrival: "5/1/2021", departure: "5/2/2021" },
+            { name: "Zoraida Westerlund", id: "XXX-12-3456", email: "Zoraida.Westerlund@example.com", level: "Iron", balance: 62,  arrival: "5/1/2021", departure: "5/2/2021" },
+            { name: "Melina Herbst", id: "XXX-12-3456", email: "Melina.Herbst@example.com", level: "Iron", balance: 141,  arrival: "5/1/2021", departure: "5/2/2021" },
+            { name: "Irma Engelhard", id: "XXX-12-3456", email: "Irma.Engelhard@example.com", level: "Silver", balance: 1369,  arrival: "5/1/2021", departure: "5/4/2021" },
+            { name: "Otilia Mccree", id: "XXX-12-3456", email: "Otilia.Mccree@example.com", level: "Iron", balance: 102,  arrival: "5/1/2021", departure: "5/2/2021" },
+            { name: "Graham Duda", id: "XXX-12-3456", email: "Graham.Duda@example.com", level: "Bronze", balance: 899,  arrival: "5/1/2021", departure: "5/2/2021" },
+            { name: "Hilario Yeatman", id: "XXX-12-3456", email: "Hilario.Yeatman@example.com", level: "Iron", balance: 7,  arrival: "5/1/2021", departure: "5/2/2021" },
         ]
 
         function formatNumber(num: number) {
             return num.toLocaleString(undefined, { minimumFractionDigits: 0});
         }
 
-        return { members, formatNumber, memberTiers }
+        const printContent = ref(null);
+
+        function printReport() {
+            // Setup print specific CSS
+            let css = "@page { size: landscape; overflow: visible !important }";
+            let style = document.createElement("style");
+            style.type = "text/css";
+            style.media = "print";
+            style.id = "printCSS";
+            style.appendChild(document.createTextNode(css));
+
+            // Create window to print
+            let printWindow = window.open("", "Print Window", "");
+
+            // As long as we were able to open the window, continue with printing
+            if (printWindow) {
+                // Write to the DOM
+                printWindow.document.write("<!DOCTYPE html><html><head>");
+
+                // Add existing styles, this is overkill but eaiser than
+                // trying to parse which classes are needed in printing
+                let styles = document.getElementsByTagName("style");
+                for (let i = 0; i < styles.length; i++) {
+                    printWindow.document.write(styles[i].outerHTML);
+                }
+                // Force landscape (only in Chrome it seems)
+                printWindow.document.head.appendChild(style);
+                printWindow.document.write("</head>");
+
+                // Print the content of this component to the window
+                printWindow.document.write(((printContent.value as unknown) as HTMLElement).innerHTML);
+                printWindow.document.write("</html>");
+
+                // Print, and close the window when the user is done.
+                printWindow.print();
+                printWindow.close();
+            }
+        }
+
+        return { members, formatNumber, memberTiers, printReport, printContent }
     }
 })
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     purge: ["./public/index.html", "./src/**/*.{vue, js, tx, jsx, tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'print': { 'raw': 'print' },
+                'screen': { 'raw': 'screen' }
+            }
+        },
     },
     variants: {
         extend: {},
