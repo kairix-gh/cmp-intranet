f83d66d6c88e300a7e3f226bd7c15c46cb8f91e1
Kairix <kairix@teameligere.com>
2021-08-25 19:50:00 -0600

    - Remove unused component - Update profile picture

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -162,7 +162,7 @@
                         <div>
                             <MenuButton class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                 <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1619016984980-703b4cf09515?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
                             </MenuButton>
                         </div>
 
